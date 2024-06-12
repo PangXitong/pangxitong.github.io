e18a6783c9c005d925ca3a5d622dd08d05b0a198
PangXitong <pangxitong@icloud.com>
2024-06-12 10:13:05 +0800

    备案信息

--- blog/page/macOS/JavaScript.js
+++
@@ -1,8 +0,0 @@
-//时间
-     function  getD1(){
-         var date=new Date();
-         var d1=date.toLocaleString();
-         document.getElementById("datetime").innerHTML =d1; 
-     }
- 
-     setInterval("getD1();",1000);
